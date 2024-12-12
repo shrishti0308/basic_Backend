f3fa704f4b5c17bdc9d529b4dd0bba76a3484c2c
Shrishti <125468465+shrishti0308@users.noreply.github.com>
2024-12-12 11:15:55 +0530

    Initialize server with Express, database connection, and error handling middleware

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const connectDB = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
+const dotenv = require("dotenv").config();
+
+connectDB();
+const app = express();
+
+const port = process.env.port || 5000;
+
+app.use(express.json());
+
+app.use(errorHandler);
+app.use("/api/contacts", require("./routes/contactRoutes"));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
